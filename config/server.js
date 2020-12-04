3b15ddff65f11af5da0a942b43875b664bfc6e89
Tingting <badonlineqq@gmail.com>
2020-12-04 12:24:56 +1100

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '18d9828ca270ea58f32279580fddd418'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '064e28ea-640a-4196-8dad-b96dbdab6a00'
+};
